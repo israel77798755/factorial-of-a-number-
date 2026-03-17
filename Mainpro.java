7185c25b124f3b7ee09f3559ed20789af3da3597
Israel Tsegaye <israeltsegaye96@gmail.com>
2026-03-17 05:57:43 +0000

    first commit

---
+++ Mainpro.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Mainpro {
+    public static void main (String[] args) {
+    
+        int x;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        System.out.println("Enter the number");
+        x = input.nextInt();
+        System.out.println("Factorial of " + x + " are:");
+
+        
+        for (int Fact = 1; Fact<= x;Fact++) {
+            
+            Fact = Fact * x;
+                System.out.print(Fact + " ");
+            }
+        }
+        
+        
+
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
