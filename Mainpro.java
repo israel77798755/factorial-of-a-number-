e20b0644fb5e0340c76b322240b2d0c5df53014f
Israel <israeltsegaye96@gmail.com>
2026-05-22 01:19:06 -0700

    Update Mainpro.java

--- Mainpro.java
+++ Mainpro.java
@@ -1,44 +1,10 @@
-import java.util.Scanner;
-public class Mainpro {
-    public static void main (String[] args) {
-    
-        int x;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number");
-        System.out.println("Enter the number");
-        x = input.nextInt();
-        System.out.println("Factorial of " + x + " are:");
+x = int(input("Enter the number: "))
 
-        
-        for (int Fact = 1; Fact<= x;Fact++) {
-            
-            Fact = Fact * x;
-                System.out.print(Fact + " ");
-            }
-        }
-        
-        
+print("Factorial of", x, "is:")
 
-        }
+fact = 1
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
+for i in range(1, x + 1):
+    fact = fact * i
+
+print(fact)
